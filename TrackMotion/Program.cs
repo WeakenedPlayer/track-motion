8cf834a79a36cea4a2fd83b3fb9f9ded1f785797
weakenedplayer <weakenedplayer@gmail.com>
2019-10-21 02:45:44 +0900

    サンプル加工後

--- TrackMotion/Program.cs
+++ TrackMotion/Program.cs
@@ -1,15 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
+using Tobii.StreamEngine;
 
 namespace TrackMotion
 {
-	class Program
+	public static class Program
 	{
+		public static void OnGazePoint(ref tobii_gaze_point_t gazePoint )
+		{
+			if (gazePoint.validity == tobii_validity_t.TOBII_VALIDITY_VALID)
+			{
+				Console.WriteLine($"Gaze point: {gazePoint.position.x}, {gazePoint.position.y}");
+			}
+		}
+		public static tobii_gaze_point_callback_t callback = OnGazePoint;
+
 		static void Main(string[] args)
 		{
+			// Create API context
+			IntPtr apiContext;
+			tobii_error_t result = Interop.tobii_api_create(out apiContext, null);
+			Debug.Assert(result == tobii_error_t.TOBII_ERROR_NO_ERROR);
+
+			// Enumerate devices to find connected eye trackers
+			List<string> urls;
+			result = Interop.tobii_enumerate_local_device_urls(apiContext, out urls);
+			Debug.Assert(result == tobii_error_t.TOBII_ERROR_NO_ERROR);
+			if (urls.Count == 0)
+			{
+				Console.WriteLine("Error: No device found");
+				return;
+			}
+
+			// Connect to the first tracker found
+			IntPtr deviceContext;
+			result = Interop.tobii_device_create(apiContext, urls[0], out deviceContext);
+			Debug.Assert(result == tobii_error_t.TOBII_ERROR_NO_ERROR);
+
+			// Subscribe to gaze data
+			result = Interop.tobii_gaze_point_subscribe(deviceContext, callback);
+			Debug.Assert(result == tobii_error_t.TOBII_ERROR_NO_ERROR);
+
+			// This sample will collect 1000 gaze points
+			for (int i = 0; i < 1000; i++)
+			{
+				// Optionally block this thread until data is available. Especially useful if running in a separate thread.
+				Interop.tobii_wait_for_callbacks( apiContext, new[] { deviceContext } );
+				Debug.Assert(result == tobii_error_t.TOBII_ERROR_NO_ERROR || result == tobii_error_t.TOBII_ERROR_TIMED_OUT);
+
+				// Process callbacks on this thread if data is available
+				Interop.tobii_device_process_callbacks(deviceContext);
+				Debug.Assert(result == tobii_error_t.TOBII_ERROR_NO_ERROR);
+			}
+
+			// Cleanup
+			result = Interop.tobii_gaze_point_unsubscribe(deviceContext);
+			Debug.Assert(result == tobii_error_t.TOBII_ERROR_NO_ERROR);
+			result = Interop.tobii_device_destroy(deviceContext);
+			Debug.Assert(result == tobii_error_t.TOBII_ERROR_NO_ERROR);
+			result = Interop.tobii_api_destroy(apiContext);
+			Debug.Assert(result == tobii_error_t.TOBII_ERROR_NO_ERROR);
 		}
 	}
 }
